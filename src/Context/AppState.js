e29b54077b5409826a2262f97104752bba214e9e
apoorva1707 <47132379+apoorva1707@users.noreply.github.com>
2022-10-11 16:58:24 +0530

    added context folder

---
+++ src/Context/AppState.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {useState,useEffect} from "react"
+import AppContext from './AppContext'
+
+export default function AppState() {
+
+    let credential = {user_name:"TeamC",password:"t3amc"}
+    let [userLoggedIn,setUserLoggedIn] = useState(false)
+    return (
+    <AppContext.Provider value={{
+        credential,userLoggedIn,setUserLoggedIn
+    }}>
+        {props.children}
+    </AppContext.Provider>
+  )
+}
