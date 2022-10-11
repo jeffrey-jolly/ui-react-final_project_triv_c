da67eca56213fbe1ccf27c9075001f8073235a2f
jeffrey-jolly <jeffryjolly@gmail.com>
2022-10-11 17:37:21 +0530

    Changes in AppState

--- src/context/AppState.js
+++ src/context/AppState.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {useState,useEffect} from "react"
 import AppContext from './AppContext'
 
-export default function AppState() {
+export default function AppState(props) {
 
     let credential = {user_name:"TeamC",password:"t3amc"}
     let [userLoggedIn,setUserLoggedIn] = useState(false)
