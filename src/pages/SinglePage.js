783c81c94ece3d922d01b9f04bb5d558ed4b933e
nandini-sa <115443651+nandini-sa@users.noreply.github.com>
2022-10-13 09:36:22 +0530

    homepage and singlepage commit

---
+++ src/pages/SinglePage.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react'
+import Logo from '../Images/logo2.PNG'
+import insta from '../Images/insta_svg.PNG'
+import facebook from '../Images/fb_svg.PNG'
+import twitter from '../Images/twitter_svg.PNG'
+import linkdin from '../Images/linkdin.PNG'
+import {Link} from 'react-router-dom'
+import AppContext from '../context/AppContext'
+
+export default function SinglePage() {
+    return(
+        <div>
+
+        </div>
+    );
+}
