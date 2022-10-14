2b804ddb64d3db3268d8f8870e1451249bd53d12
nandini-sa <115443651+nandini-sa@users.noreply.github.com>
2022-10-14 11:56:03 +0530

    homepage_done_commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect,useState } from 'react'
 import Logo from '../Images/logo2.PNG'
 import insta from '../Images/insta_svg.PNG'
 import facebook from '../Images/fb_svg.PNG'
@@ -6,24 +6,46 @@ import twitter from '../Images/twitter_svg.PNG'
 import linkdin from '../Images/linkdin.PNG'
 import {Link} from 'react-router-dom'
 import AppContext from '../context/AppContext'
-
-
+import ProductCard from './ProductCard'
+import loadingGif from '../Images/tenor.gif'
 
 export default function Dashboard() {
   let appContext = useContext(AppContext)
+  let [blogs,setBlogs] = useState([])
+  let [loading,setLoading] = useState(true);
+  const apiURL = "http://localhost:3000/posts"
+
+  async function getNewBlog() {
+    let response = await fetch(apiURL)
+    console.log("Response", response)
+    if (response.ok) {
+        let result = await response.json()
+        console.log("Result", result)
+        setBlogs(result)
+        setLoading(false)
+
+    } else {
+        console.log("Error")
+        setLoading(false)
+    }
+
+}
+useEffect(() => {
+    getNewBlog()
+}, [])
+
+  
   return (
-    <>
+    <div className="max-h-fit">
         <header className=" w-auto h-20 bg-black  text-white flex flex-row justify-between z-10">
             <img src={Logo} className='w-30 h-full ml-10'/>
             <button className='mr-10 h-1/2 mt-4 p-2 hover:text-lg hover:rounded-2xl  hover:bg-gray-500' onClick={()=>{appContext.setUserLoggedIn(false)}}>LogOut</button>
         </header>
         <div className="flex flex-wrap ">
-        {/* <!-- Side bar --> */}
-        <aside className="text-xl bg-black min-h-screen w-1/6 shadow-2xl text-white  flex flex-col align-bottom justify-between ">
+        <aside className="text-xl bg-black max-h-fit w-1/6 shadow-2xl text-white  flex flex-col align-bottom justify-between ">
         
             <ul className="top-nav"> 
-                {/* <li><img src="images/spotifylogo.PNG" class="main-logo"></li>
-                <li><img src="images/spotify-icon.PNG" class="mobile-logo"></li> */}
+                
                 
                 <li className="p-5 font-bold flex flex-row">
                     
@@ -126,8 +148,12 @@ export default function Dashboard() {
 
         <section className=' max-w-7xl mx-auto flex flex-col justify-between w-1/4 '>
                 <div className='border border-transparent shadow-lg mr-4 mt-2 p-8  rounded-md flex flex-co justify-between hover:shadow-2xl hover:border hover:bg-yellow-200'>
-                  <div>
-                    <p>NUMBER OF POSTS</p>
+                  <div className='flex flex-row'>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+</svg>
+
+                    <p className='ml-2'>NUMBER OF POSTS</p>
                   </div>
                   {/* <div>
                     <p>bye</p>
@@ -140,8 +166,12 @@ export default function Dashboard() {
 
         <section className=' max-w-7xl mx-auto flex flex-col  w-1/4 '>
                 <div className=' border border-transparent shadow-lg mr-4 mt-4 p-8  rounded-md flex flex-col justify-between hover:shadow-2xl hover:border hover:bg-yellow-200'>
-                  <div>
-                    <p>STATISTICS</p>
+                  <div className='flex flex-row'>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
+</svg>
+
+                    <p className='ml-2'>STATISTICS</p>
                   </div>
                   {/* <div>
                     <p>bye</p>
@@ -154,8 +184,12 @@ export default function Dashboard() {
 
         <section className=' max-w-7xl mx-auto flex flex-col w-1/4 h-1/3 '>
                 <div className='border border-transparent shadow-lg mr-4 mt-4 p-8 rounded-md flex flex-col justify-between hover:shadow-2xl hover:border hover:bg-yellow-200'>
-                  <div>
-                    <p>CATEGORIES</p>
+                  <div className='flex flex-row'>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 01-1.125-1.125v-3.75zM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-8.25zM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 01-1.125-1.125v-2.25z" />
+</svg>
+
+                    <p className='ml-2'>CATEGORIES</p>
                   </div>
                   {/* <div>
                     <p>bye</p>
@@ -178,7 +212,17 @@ export default function Dashboard() {
                       <button className='mr-2  p-1 w-fit h-fit text-sm text-white rounded-md bg-black hover:bg-gray-400'>CREATE POSTS</button></Link>
                   </div>
                   <div>
-                      <p>posts</p>
+                      <p>
+                      <div className='max-w-7xl mx-auto flex max-h-fit flex-col bg-red-400 justify-between p-4 '>
+                    {loading? (<Loader/>):(
+                        blogs.map((blog)=>{
+                            return(
+                            <ProductCard key={blog.id} blog={blog}/>
+                            )
+                        })
+                    )}
+                </div>
+                      </p>
                   </div>
                 </div>
                   
@@ -263,7 +307,15 @@ export default function Dashboard() {
 
         </main> */}
     </div>
-    </>
+    </div>
     
   )
+}
+
+function Loader() {
+  return (
+    <>
+      <img src={loadingGif} alt="Loading" className="w-24 mx-auto mt-12" />
+    </>
+  );
 }
--- src/pages/ProductCard.js
+++ src/pages/ProductCard.js
@@ -7,14 +7,13 @@ export default function ProductCard(props) {
 //   let appContext = useContext(AppContext)
   return (
   
-
-        <div className="w-1/4 border border-transparent shadow-lg mr-4 mt-4 p-8 rounded-md flex flex-col justify-between hover:shadow-2xl hover:border hover:bg-yellow-200">
-      {/* <img src={props.blog.image} className="h-64 mx-auto" /> */}
-      {/* <Link to={`/products/${props.blog.id}`}> */}
-        <h3 className='text-2xl font-bold text-green-500 my-4'>{props.blog.title}</h3>
-      {/* <p>Category : {props.item.category?props.item.category:"No Category Available"}</p> */}
-      <h3 className='text-2xl font-bold text-green-500 my-4'>{props.blog.categoryId}</h3>
-      <h3 className='text-2xl font-bold text-green-500 my-4'>{props.blog.createdAt}</h3>
+        
+        <div className="w-2/5 border border-transparent shadow-lg mr-4 mt-4 p-8 rounded-md flex flex-row justify-between hover:shadow-2xl hover:border hover:bg-black hover:bg-opacity-70">
+          <pre>
+            <h3 className='text-sm font-bold text-white my-4'>Title : {props.blog.title}</h3>
+            <h3 className='text-sm  text-white my-4'>Blog ID : {props.blog.categoryId}</h3>
+            <h3 className='text-sm  text-white my-4'>Created on : {props.blog.createdAt}</h3>
+          </pre>
       </div>
         
 
