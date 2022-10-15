e41b32be5070a0f7ce9a8c9a80a84a470d5be4bd
jeffrey-jolly <jeffryjolly@gmail.com>
2022-10-15 20:35:39 +0530

    header logout user-auth

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -11,9 +11,11 @@ export default function Header(){
     <div>
         <header className=" w-auto h-14 bg-black  text-white flex flex-row justify-between items-center">
             <img src={Logo} className='w-30 h-full ml-3 p-1 cursor-pointer' onClick={()=>{navigate('/')}}/>
+          {appContext.userLoggedIn?(<>
             <button className='mr-10 h-fit w-fit p-2 text-center border border-slate-50 rounded-full hover:rounded-full  hover:scale-105 hover:bg-white hover:text-black font-semibold'
              onClick={()=>{appContext.handleLoginStatus(false)
              navigate('/')}}>Logout</button>
+          </>):(<></>)}  
         </header>
     </div>
   )
