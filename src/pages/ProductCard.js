2294629d130dc36355b91d9666b9854f970a1cfb
jeffrey-jolly <jeffryjolly@gmail.com>
2022-10-16 19:58:55 +0530

    minor updates for homen

--- src/pages/ProductCard.js
+++ src/pages/ProductCard.js
@@ -13,8 +13,9 @@ export default function ProductCard(props) {
         <div className="w-full h-fit border border-transparent shadow-lg mr-4 mt-2 p-4 rounded-md flex flex-row justify-between hover:shadow-2xl hover:border hover:bg-green-200 hover:bg-opacity-70">
          <Link to={`/single-page/${props.blog.id}`} ><pre  onClick={()=>{appContext.handleViewCount()}}>
             <h3 className='text-sm font-bold text-white my-4'>{props.blog.title}</h3>
-            <h3 className='text-sm  text-white my-4 italic'>Category : {props.blog.category}</h3>
-            <h3 className='text-sm  text-white my-4'>Created on : {props.blog.createdAt}</h3>
+            <h3 className='text-sm  text-white my-4 italic'>{props.blog.category}</h3>
+            <h3 className='text-sm  text-white my-4 underline'>{props.blog.createdAt}</h3>
+
           </pre>
           </Link> 
       </div>
