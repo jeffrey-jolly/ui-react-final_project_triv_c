59bcbc11e661d893148101f50e33b443fe56bbc3
nandini-sa <115443651+nandini-sa@users.noreply.github.com>
2022-10-16 23:33:35 +0530

    commit_again

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,68 +3,70 @@ import Logo from '../Images/logo2.PNG'
 import AppContext from '../context/AppContext'
 import { useNavigate } from 'react-router-dom'
 import aboutback from '../Images/about_us_back.jpg'
+import Header from './Header'
 
 
-export default function Header(){
+export default function AboutUs(){
   let appContext = useContext(AppContext)
   const navigate = useNavigate()
   return(
     <div>
-        <header className=" w-full h-20 bg-black  text-white sticky">
-            {/* <img src={Logo} className='w-30 h-full ml-10 cursor-pointer' onClick={()=>{navigate('/')}}/>
-            <button className='mr-10 h-fit w-fit p-2 text-center border border-slate-50 rounded-full hover:rounded-full  hover:bg-gray-500'
-             onClick={()=>{appContext.handleLoginStatus(false)
-             navigate('/')}}>Logout</button> */}
-             <p className="flex flex-col justify-center items-center text-3xl font-bold pt-5">ABOUT US</p>
-             {/* <img src={aboutback} className="pt-10 h-60 w-full"/> */}
-             
-        </header>
+        <Header/>
+
+        
         <div className='m-10'>
-            <div className='font-bold h-10 m-5 pl-14 pr-8 text-center'>
+            <div className=' h-10 m-5 pl-14 text-sm pr-8 text-center'>
                 
-            Where do I start my blog? What is the best blog site to earn money? These may be your first questions in your blogging journey.
+            <p className='italic'>It all started when we were assigned a group on monday morning.From knowing little to learning along the way, we came a long way with the help of our wonderful trainer <strong>Arjit Verma</strong>.  We started with splitting the workforce efficiently among our team members. This app is an implementation of a simple blogging website using various React tools like Context API, Redux, Formik, Yup, html-react-parser, quill, Tailwind CSS,etc...
+            We are extremely grateful to our mentor, colleagues, friends and family for supporting us throughout our journey.
+            </p><br/>
+            <div className='mt-4 font-bold inline-flex justify-center h-10 w-full mx-auto hover:cursor-pointer'>
+                
+                <h1>CLICK HERE TO VIEW OUR GITHUB PAGE</h1><a href="https://github.com/jeffrey-jolly/ui-react-final_project_triv_c" target={'_blank'}>
+                <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6  hover:text-blue-600">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
+</svg>
+</span>
+                </a>
+                
 
-It really is an important question and lays the building blocks of your blog. It can affect your frustration level, the speed and performance of your blog, and your ability to customize it.
-
-Not to mention, you need to pick the best blog site for making money!
-
-Therefore, choosing the best blogging platform and site for your blog in the beginning is a must-do on your blog-starting journey.
-            <div>
-                <a href="https://github.com/jeffrey-jolly/ui-react-final_project_triv_c" className=''>CLICK HERE TO GO TO OUR GITHUB LINK</a>
+                
+                
+                
             </div>
                 
-                
+        
             </div>
             
         <div className='max-w-7xl h-screen mx-auto flex flex-wrap justify-between pt-16'>
-                <div className='w-1/4 m-10 shadow-md mt-50 grid place-items-center font-bold hover:bg-black hover:bg-opacity-10 '>
-                    ABHIRAMI CHANDRAMOULI
+                <div className='w-1/4 m-10 shadow-xl mt-50 rounded-lg flex flex-col text-center justify-center font-bold hover:bg-violet-400 hover:bg-opacity-10 '>
+                    <h1>ABIRAMI CHANDRAMOULI</h1>
+                    <p>(Associate Software Engineer)</p>
+                    
+                </div>
+                <div className='w-1/4 m-10 shadow-xl mt-50 rounded-lg flex flex-col text-center justify-center font-bold hover:bg-green-400 hover:bg-opacity-10'>
+                    <h1>APOORVA ANANTH P</h1>
                     <p className='text-sm '>(Associate Software Engineer)</p>
                     
                 </div>
-                <div className='w-1/4 m-10 shadow-md mt-50 grid place-items-center font-bold hover:bg-black hover:bg-opacity-10'>
-                    APOORVA ANATH
+                <div className='w-1/4 m-10 shadow-xl mt-50 rounded-lg flex flex-col text-center justify-center font-bold hover:bg-red-400 hover:bg-opacity-10'>
+                    <h1>JEFFREY JOLLY</h1>
                     <p className='text-sm '>(Associate Software Engineer)</p>
                     
                 </div>
-                <div className='w-1/4 m-10 shadow-md mt-50 grid place-items-center font-bold hover:bg-black hover:bg-opacity-10'>
-                    JEFFREY JOLLY
-                    <p className='text-sm '>(Associate Software Engineer)</p>
-                    
-                </div>
-                <div className='w-1/4 m-10 shadow-md mt-50 grid place-items-center font-bold hover:bg-black hover:bg-opacity-10'>
-                    MUNDAN PUDUSHERRY DENSY JOY
+                <div className='w-1/4 m-10 shadow-xl mt-50 rounded-lg flex flex-col text-center justify-center font-bold hover:bg-blue-400 hover:bg-opacity-10'>
+                    <h1>MUNDAN PUTHUSSERY DENSY JOY</h1>
                     <p className='text-sm '>(Associate Software Engineer)</p>
 
                     
                 </div>
-                <div className='w-1/4 m-10 shadow-md mt-50 grid place-items-center font-bold hover:bg-black hover:bg-opacity-10'>
-                    NAMITH N
+                <div className='w-1/4 m-10 shadow-xl mt-50 rounded-lg flex flex-col text-center justify-center font-bold hover:bg-yellow-400 hover:bg-opacity-10'>
+                    <h1>NAMITH N</h1>
                     <p className='text-sm '>(Associate Software Engineer)</p>
                     
                 </div>
-                <div className='w-1/4 m-10 shadow-md mt-50 grid place-items-center font-bold hover:bg-black hover:bg-opacity-10'>
-                    NANDINI SAJEEV
+                <div className='w-1/4 m-10 shadow-xl mt-50 rounded-lg flex flex-col text-center justify-center font-bold hover:bg-orange-600 hover:bg-opacity-10'>
+                    <h1>NANDINI SAJEEV</h1>
                     <p className='text-sm '>(Associate Software Engineer)</p>
                 </div>
                 
