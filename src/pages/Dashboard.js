d4e2de4e5f3e1b015a49c62f1602162d442c7ec0
apoorva1707 <47132379+apoorva1707@users.noreply.github.com>
2022-10-17 13:06:01 +0530

    dropdown added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -12,7 +12,8 @@ import { useNavigate } from 'react-router-dom'
 import Header from './Header'
 import parse from 'html-react-parser'
 import toast from 'react-hot-toast';
-import PieChart from './PieChart'
+import PieChart from './PieChart';
+import '../App.css'
 
 
 
@@ -129,10 +130,20 @@ export default function Dashboard() {
                 <svg className="nav-icon w-6 h-6 hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                 </svg>
+                <div className="dropdown1">
                 <span>
                   Categories
                 </span>
-
+                <div class="dropdown-content">
+                <a href="#">Entertainment</a>
+               <a href="#">Food</a>
+               <a href="#">Technology</a>
+               <a href="#">Your Space</a>
+               <a href="#">Health and Fitness</a>
+  
+                  
+                  </div>
+                </div>
               </li>
 
             </ul>
